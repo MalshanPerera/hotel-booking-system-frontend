31e06495bb0a593270bad32648753eeadd5571ef
MalshanPerera <malshanperera3@gmail.com>
2022-07-06 21:14:49 +0530

    HomePage Lists

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,8 @@
+import Featured from '../../components/featured/Featured';
+import FeaturedProperty from '../../components/featuredProperty/FeaturedProperty';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import Property from '../../components/property/Property';
 import styles from './Home.module.scss';
 
 const Home = () => {
@@ -7,6 +10,13 @@ const Home = () => {
     <div>
       <Navbar />
       <Header />
+      <div className={styles.homeContainer}>
+        <Featured />
+        <h1 className={styles.homeTitle}>Browse by property type</h1>
+        <Property />
+        <h1 className={styles.homeTitle}>Homes guests love</h1>
+        <FeaturedProperty />
+      </div>
     </div>
   );
 };
